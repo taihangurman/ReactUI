fb6f8edd5fbebf76a4634adc6530f09d502005e0
Taihan Gurman <taihangurman56219@gmail.com>
2021-07-26 06:09:26 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "./layout/index.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/index.js
@@ -0,0 +1,224 @@
+import React from "react";
+import "./styles.css";
+
+const ZX = () => {
+    return (
+        <div>
+            <div className="th-head-one">
+                <div className="th-one-content">
+                    <div>
+                        <h2>text</h2>
+                        <div>
+                            <h2>text</h2>
+                            <h2>text</h2>
+                        </div>
+                    </div>
+                    <div>
+                        <button>
+                            button
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="th-head-two">
+                <div className="th-two-cnt1">
+                    <h2>text</h2>
+                    <h2 style={{ marginRight: '20px' }}>text</h2>
+                </div>
+                <div className="th-two-cnt2">
+                    <div className="th-two-cnt2-div">
+                        <h2>text</h2>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                </div>
+
+                <div className="th-two-cnt2">
+                    <div className="th-two-cnt2-div">
+                        <h2>text</h2>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                    <div>
+                        <h2>text</h2>
+                    </div>
+                </div>
+
+                <div className="th-two-progress-bar">
+                    <div className="progress-bar1">
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <h2>text</h2>
+                            <h2>text</h2>
+
+                        </div>
+                        <div>
+                            <progress className="progress" value="75" max="100"> 32% </progress>
+                            <span>75%</span>
+                        </div>
+
+                    </div>
+                    <div className="progress-bar2">
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <h2>text</h2>
+                            <h2>text</h2>
+                        </div>
+                        <div >
+                            <progress className="progress" value="30" max="100"> 32% </progress>
+                            <span>32%</span>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="th-head-three">
+                <div className="th-three-content">
+                    <div>
+                        <label for="text">text:</label>
+                        <input type="date"></input>
+                        <a>~</a>
+                        <input type="date"></input>
+                    </div>
+                    <div>
+                        <label for="text" >text:</label>
+                        <input type="text" placeholder="input"></input>
+                    </div>
+                    <div>
+                        <label for="text" >text:</label>
+                        <button>btn</button>
+                        <button>btn</button>
+                        <button>btn</button>
+                        <button>btn</button>
+                        <button>btn</button>
+                        <button>btn</button>
+                        <button>btn</button>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+      
+       <div className="th-head-four">
+           <div style={{display:'flex', justifyContent: 'flex-end'}}>
+               <button>button</button>
+               <button>button</button>
+           </div>
+           <div className="th-four-content">
+               <div>
+                   <input type="checkbox" />
+                   <h3 style={{display: 'inline',marginLeft: '10px'}}>text</h3>
+               </div>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+
+           </div>
+           <div className="th-four-content">
+               <div>
+                   <input type="checkbox" />
+                   <h3 style={{display: 'inline',marginLeft: '10px'}}>text</h3>
+               </div>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3 style={{border: '6px solid green', padding: '0 16px', margin: '3px 0'}}>text</h3>
+
+           </div>
+           <div className="th-four-content">
+               <div>
+                   <input type="checkbox" />
+                   <h3 style={{display: 'inline',marginLeft: '10px'}}>text</h3>
+               </div>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3 style={{border: '6px solid green', padding: '0 16px', margin: '3px 0'}}>text</h3>
+
+           </div>
+           <div className="th-four-content">
+               <div>
+                   <input type="checkbox" />
+                   <h3 style={{display: 'inline',marginLeft: '10px'}}>text</h3>
+               </div>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3 style={{border: '6px solid green', padding: '0 16px', margin: '3px 0'}}>text</h3>
+
+           </div>
+           <div className="th-four-content">
+               <div>
+                   <input type="checkbox" />
+                   <h3 style={{display: 'inline',marginLeft: '10px'}}>text</h3>
+               </div>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3 style={{border: '6px solid green', padding: '0 16px', margin: '3px 0'}}>text</h3>
+
+           </div>
+           <div className="th-four-content">
+               <div>
+                   <input type="checkbox" />
+                   <h3 style={{display: 'inline',marginLeft: '10px'}}>text</h3>
+               </div>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3>text</h3>
+                   <h3 style={{border: '6px solid green', padding: '0 16px', margin: '3px 0'}}>text</h3>
+
+           </div>
+
+       </div>
+        </div>
+    )
+}
+
+export default ZX;
